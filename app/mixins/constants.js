e656d2661713cae72a3da960c6e716a9c8379df0
jp-999 <jayanthpeddi999@gmail.com>
2025-04-11 23:07:50 +0530

    stage 16

--- app/mixins/constants.js
+++ app/mixins/constants.js
@@ -1,11 +1,9 @@
-// Define a set of constants for peer connection statuses
 const Constants = Object.freeze({
-  PENDING: 'pending', // Status indicating that the connection is pending
-  HANDSHAKE_RECEIVED: 'handshake received', // Status indicating that a handshake has been received
-  UNCHOKE_RECEIVED: 'unchoke received', // Status indicating that an unchoke message has been received
+  PENDING: 'pending',
+  HANDSHAKE_RECEIVED: 'handshake received',
+  UNCHOKE_RECEIVED: 'unchoke received',
 });
 
-// Export the constants as part of an object for use in other modules
 module.exports = {
-  PeerConnectionStatus: Constants, // Export the peer connection statuses
+  PeerConnectionStatus: Constants,
 };
